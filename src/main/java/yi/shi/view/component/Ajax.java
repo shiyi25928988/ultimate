2b73f280b61434359a3dfba5df16e2209bf81500
DESKTOP-3I6EHQ5\shiyi <shiyi25928988@163.com>
2025-10-30 13:25:10 +0800

    注册

--- src/main/java/yi/shi/view/component/Ajax.java
+++ src/main/java/yi/shi/view/component/Ajax.java
@@ -21,6 +21,21 @@ public class Ajax {
         return getAjaxFunction(url, "POST", data);
     }
 
+    public static String getAjax(String url, Object  data, String callbackUrl){
+        return getAjaxFunction(url, "GET", JsonUtils.toJson(data), callbackUrl);
+    }
+    public static String getAjax(String url, String  data, String callbackUrl){
+        return getAjaxFunction(url, "GET", data, callbackUrl);
+    }
+
+    public static String postAjax(String url, Object  data, String callbackUrl){
+        return getAjaxFunction(url, "POST", JsonUtils.toJson(data), callbackUrl);
+    }
+
+    public static String postAjax(String url, String  data, String callbackUrl){
+        return getAjaxFunction(url, "POST", data, callbackUrl);
+    }
+
     private static String getAjaxFunction(String url, String method, String data){
         String ajax =
                 """
@@ -47,8 +62,38 @@ public class Ajax {
                     }
                 });
                 """.formatted(url, method, data);
+        return ajax;
+    }
 
-        log.info("ajax: {}", ajax);
+    private static String getAjaxFunction(String url, String method, String data, String callbackUrl){
+        String ajax =
+                """
+                    $.ajax({
+                    url: '%s',
+                    method: '%s',
+                    data: JSON.stringify(%s),
+                    success: function(response) {
+                        if (response.code === 1) {
+                            M.toast({
+                                html: response.message,
+                             });
+                             setTimeout(function() {
+                                window.location.href = '%s';
+                             }, 1000);
+                        } else if (response.code === 0) {
+                             M.toast({
+                                 html: response.message,
+                             });
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                             M.toast({
+                                 html: xhr.statusText,
+                             });
+                        console.error('请求失败:', error);
+                    }
+                });
+                """.formatted(url, method, data, callbackUrl);
         return ajax;
     }
 }
--- src/main/java/yi/shi/view/pages/UserRegistryPage.java
+++ src/main/java/yi/shi/view/pages/UserRegistryPage.java
@@ -2,6 +2,7 @@ package yi.shi.view.pages;
 
 import j2html.tags.ContainerTag;
 import j2html.tags.specialized.*;
+import lombok.extern.slf4j.Slf4j;
 import yi.shi.view.Page;
 import yi.shi.view.element.Button;
 import yi.shi.view.element.Input;
@@ -15,6 +16,7 @@ import yi.shi.plinth.http.result.HTML;
 
 import static j2html.TagCreator.*;
 
+@Slf4j
 @HttpService
 public class UserRegistryPage extends Page {
 
@@ -51,23 +53,37 @@ public class UserRegistryPage extends Page {
                         h4().withClass("center-align").withText("用户注册"),
                         form
                 ),
-                script(("$(document).ready(function() {\n" +
-                        "                    $('#registrationForm').submit(function(event) {\n" +
-                        "                        event.preventDefault();\n" +
-                        "                        var username = $('#username').val();\n" +
-                        "                        var email = $('#email').val();\n" +
-                        "                        var password = $('#password').val();\n" +
-                        "                        var confirmPassword = $('#confirmPassword').val();\n" +
-                        "\n" +
-                        "                        if (password !== confirmPassword) {\n" +
-                        "                            alert('密码不匹配');\n" +
-                        "                            return;\n" +
-                        "                        }\n" +
-                        "\n" +
-                        "                        // 发送 AJAX 请求\n" + Ajax.postAjax("/api/user/add", "{username: username, email: email, passwd: password}") +
-                        "                    });\n" +
-                        "                });")
-                ));
+                script("""
+                        $(document).ready(function() {
+                            $('#registrationForm').submit(function(event) {
+                                event.preventDefault();
+                                var username = $('#username').val();
+                                var email = $('#email').val();
+                                var password = $('#password').val();
+                                var confirmPassword = $('#confirmPassword').val();
+                                if (username == null || username == ''){
+                                    M.toast({
+                                        html: '用户名不能为空',
+                                    });
+                                    return;
+                                }
+                                if (password == null || password == ''){
+                                    M.toast({
+                                        html: '密码不能为空',
+                                    });
+                                    return;
+                                }
+                                if (password !== confirmPassword) {
+                                    M.toast({
+                                        html: '两次输入密码不一致',
+                                    });
+                                    return;
+                                }
+                                %s
+                        });});
+                        """.formatted(Ajax.postAjax("/api/user/add", "{username: username, email: email, passwd: password}", "/page/login"))
+                )
+        );
         return main;
     }
 
